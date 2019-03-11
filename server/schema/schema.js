ffc0358cc07d37ec9cd5e284e7a9d7692aa05e20
webstar0316 <webstar0316@outlook.com>
2019-03-11 22:17:04 +1000

    some fix

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -21,13 +21,17 @@ var authors = [
   { name: 'Brandon Sanderson', age: 42, id: '2' },
   { name: 'Terry Pratchett', age: 66, id: '3' },
 ];
-
+ 
 const BookType = new GraphQLObjectType({
   name: 'Book',
   fields: () => ({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
-    genre: { type: GraphQLString }
+    genre: { type: GraphQLString },
+    author: {
+      type: AuthorType,
+      resolve()
+    }
   })
 });
 
